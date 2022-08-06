fc8fc7582465e23002d5836655918f208f43b619
Ramakrishna Thadivaka <thadivaka,ramakrishna@gmail.com>
2022-08-06 23:58:36 +0530

    Added code changes for promotion rule added uni test failed working on chnageging the method to pass.

--- VirtualBasketPricing/Pricing/CalculatePrice.cs
+++ VirtualBasketPricing/Pricing/CalculatePrice.cs
@@ -13,36 +13,62 @@ namespace VirtualBasketPricing
         public CalculatePrice(IEnumerable<Rule> rules)
         {
             _rules = rules;
-            
+            LoadItemsWithCount(rules);
         }
         public int GetTotalPrice(IList<string> items)
         {
             int totalPrice = 0;
-
-            foreach (var item in items)
+            foreach (var item in rulesDict)
             {
-                var itemsToBuy = _rules.Where(r => r.ItemName == item).Select(x => x.NumberOfItemToBuy).FirstOrDefault();
-                var itemsFree = _rules.Where(r => r.ItemName == item).Select(x => x.NumberItemsForFree).FirstOrDefault();
-                var itemPrice = _rules.Where(r => r.ItemName == item).Select(x => x.Price).FirstOrDefault();
-                var totalItems = itemsFree + itemsToBuy;
-                var itemCount = items.Count;
-                if (itemCount == totalItems)
+                var itemCount = 0;
+                var getRuleItems = item.Value;
+                foreach (var ruleItem in getRuleItems)
                 {
-                    var quoientValue = (itemCount / totalItems) * itemsToBuy;
-                    var remainderValue = itemCount % totalItems;
+                    itemCount += items.Where(i => i == ruleItem).Count();
+                }
 
-                    var totalItemsToCalculate = (quoientValue + remainderValue) * itemPrice;
+                var numberOfPromotionItems = item.Key.NumberItemsForFree + item.Key.NumberOfItemToBuy;
+                var amount = _rules.Where(r => r.NumberItemsForFree == item.Key.NumberItemsForFree &&
+                                                    r.NumberOfItemToBuy == item.Key.NumberOfItemToBuy).Select(rs => rs.Price).FirstOrDefault();
 
-                    totalPrice += totalItemsToCalculate;
-                    break;
+                if(numberOfPromotionItems > 0 && itemCount > numberOfPromotionItems)
+                {
+                    var quoientValue = (itemCount / numberOfPromotionItems) * item.Key.NumberOfItemToBuy;
+                    var remainderValue = itemCount % numberOfPromotionItems;
+
+                    var totalItemsToCalculate = quoientValue + remainderValue;
+
+                    totalPrice += totalItemsToCalculate * amount;
                 }
                 else
                 {
-                    totalPrice += itemCount * itemPrice;
-                    break;
+                    totalPrice += itemCount * amount;
                 }
             }
+
             return totalPrice;
         }
+
+        #region Private Methods
+        private void LoadItemsWithCount(IEnumerable<Rule> rules)
+        {
+            foreach (var rule in rules)
+            {
+                if (rulesDict.Any(d => d.Key.NumberOfItemToBuy == rule.NumberOfItemToBuy && 
+                d.Key.NumberItemsForFree == rule.NumberItemsForFree))
+                {
+                    var itemList = rulesDict.Where(d => d.Key.NumberItemsForFree == rule.NumberItemsForFree &&
+                    d.Key.NumberOfItemToBuy == rule.NumberOfItemToBuy).Select(d => d.Value).ToList().FirstOrDefault();
+
+                    itemList.Add(rule.ItemName);
+                }
+                else
+                {
+                    RuleItem ruleItem = new RuleItem { NumberOfItemToBuy = rule.NumberOfItemToBuy, NumberItemsForFree = rule.NumberItemsForFree };
+                    rulesDict[ruleItem] = new List<string> { rule.ItemName };
+                }
+            }
+        }
+        #endregion
     }
 }
